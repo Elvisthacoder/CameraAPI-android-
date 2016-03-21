d3a9a7cc9fd528cde274ac3c9f969bf3d316b11a
Elvisthacoder <ekk213@gmail.com>
2016-03-21 12:21:05 +0300

    adding the sdcard folder

--- app/src/main/java/com/example/taifa/camera/MainActivity.java
+++ app/src/main/java/com/example/taifa/camera/MainActivity.java
@@ -2,43 +2,48 @@ package com.example.taifa.camera;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
+
+    Button button;
+    ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.Image_view);
+        button.setOnClickListener(new View.OnClickListener()    {
 
+            @Override
+            public void OnClick(View v){
+
+
+
+            }
+
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    Private File getFile() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        File folder = new File ("sdcard/camera_app");
+        if(!folder.exists()){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+            folder.mkdir();
+
+
         }
 
-        return super.onOptionsItemSelected(item);
+
+        return null;
     }
 }
