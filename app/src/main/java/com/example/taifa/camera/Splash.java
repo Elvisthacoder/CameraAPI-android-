e2a01dfb551c59dd1e0835da7af45b636491e576
Elvisthacoder <ekk213@gmail.com>
2016-03-21 15:53:59 +0300

    adding splash activity layout

--- app/src/main/java/com/example/taifa/camera/Splash.java
+++ app/src/main/java/com/example/taifa/camera/Splash.java
@@ -1,7 +1,30 @@
 package com.example.taifa.camera;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by taifa on 3/21/16.
  */
-public class Splash {
+//public class Splash {
+//}
+public class Splash extends AppCompatActivity {
+    private final int SplashDisplayLength = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Intent that will start the Main Activity
+                Intent mainIntent = new Intent(Splash.this, Main.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SplashDisplayLength);
+    }
 }
